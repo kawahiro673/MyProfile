3d9ba7a1ae8a034f50c18956cbd2d81f196960b2
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-09-09 20:02:08 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+console.log('node.js');
+var express = require('express'); //Express使う定型分
+var app = express(); //expressオブジェクトでappインスタンス作成
+const http = express('http');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('サーバー接続成功');
+});
