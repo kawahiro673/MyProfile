9505c1508248d36722b2d7ad665ebbaf45c2ee69
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-09-10 00:28:05 +0900

    Initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,53 @@
 const submitButton = document.getElementById('submitButton');
+const nameValue = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const textarea = document.getElementById('email').value;
+const message = document.getElementById('message');
 
 submitButton.addEventListener('click', function (event) {
   event.preventDefault();
-  alert('ボタンがクリックされました！');
+
+  //未入力
+  if (nameValue === '' || email === '' || textarea === '') {
+    message.style.display = 'block';
+    message.style.color = 'red';
+    message.innerHTML = '入力されていない情報があります';
+    return false;
+  }
+
+  //メールアドレスに何か入力されていれば、バリデーションチェック
+  if (authEmail.value !== '' && !validateEmail(authEmail.value)) {
+    message.style.display = 'block';
+    message.style.color = 'red';
+    message.innerHTML = 'メールアドレスの入力に誤りがあります';
+    return false;
+  }
+
+  //   $.ajax({
+  //     url: '/contact/',
+  //     type: 'POST',
+  //     dataType: 'Json',
+  //     contentType: 'application/json',
+  //     data: JSON.stringify({
+  //       nameValue,
+  //       email,
+  //       textarea,
+  //     }),
+  //     success: function (res) {
+  //       message.style.display = 'block';
+  //       message.style.color = 'black';
+  //       message.innerHTML = '送信されました';
+
+  //     },
+  //   });
 });
+
+// 通常のメールアドレスバリデーション関数
+export const validateEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  // メールアドレスのフォーマットをチェック
+  if (!emailRegex.test(email)) {
+    return false;
+  }
+  return true;
+};
